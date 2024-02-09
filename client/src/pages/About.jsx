50fd750adf809915dd9e0b718b54f0b6e52ccee4
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2024-02-09 11:41:20 -0800

    About page with founder photos and titles

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Founder1Image from '../assets/founders/founder1.jpg'; // Import the image for Founder 1
-import Founder2Image from '../assets/'; // Import the image for Founder 2
+import Founder1Image from '../assets/Founders/AnnaFounder.jpg'; // Import the image for Founder 1
+import Founder2Image from '../assets/Founders/AmandaFounder.jpg'; // Import the image for Founder 2
 
 const AboutPage = () => {
   return (
@@ -10,7 +10,8 @@ const AboutPage = () => {
       <div className="founder">
         <img src={Founder1Image} alt="Founder 1" />
         <div className="bio">
-          <h2>Founder 1 Name</h2>
+          <h2>Anna Rose Benedetti</h2>
+          <h3>Founder and Curator</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in mauris ut ante dapibus fringilla. 
             Fusce ut nisi in urna cursus blandit. Duis euismod augue a risus efficitur, in volutpat quam bibendum.
@@ -21,7 +22,8 @@ const AboutPage = () => {
       <div className="founder">
         <img src={Founder2Image} alt="Founder 2" />
         <div className="bio">
-          <h2>Founder 2 Name</h2>
+          <h2>Amanda Ireton</h2>
+          <h3>Founder and Curator</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in mauris ut ante dapibus fringilla. 
             Fusce ut nisi in urna cursus blandit. Duis euismod augue a risus efficitur, in volutpat quam bibendum.
