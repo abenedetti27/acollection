f1b19105f74c8806376f5a40f937f83a70216ffb
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2024-03-18 15:16:08 -0700

    Update server port in server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -37,8 +37,9 @@ const startApolloServer = async () => {
 
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(`Server is running on port ${PORT}`);
+}).on('error', (e) => {
+  console.error('Error starting server:', e);
     });
   });
 };
